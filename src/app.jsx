15e930f6bd58756446f58c01e2f4d2462b22ae9f
Natalia Crass <natalia@thorium.local>
2017-03-27 22:34:04 +0200

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,64 @@
+import React, { Component}  from 'react';
+import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
+import './app.css';
+import Profile from './profile';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      artist: null
+    }
+  }
+
+search() {
+  console.log('this.state', this.state);
+  const BASE_URL = 'https://api.spotify.com/v1/search?';
+  const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit1`;
+  console.log('FETCH_URL', FETCH_URL);
+  fetch(FETCH_URL, {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(json => {
+    const artist = json.artists.items[0];
+    console.log('artist', artist);
+    this.setState({artist});
+  });
+
+}
+  render() {
+    return (
+      <div className="App">
+        <div className="App-title">Music Master</div>
+        <FormGroup>
+          <InputGroup>
+            <FormControl type="text" placeholder="Search for an Artist" value={this.state.query} onChange={event => {
+              this.setState({query: event.target.value})
+            }}
+            onKeyPress={event => {
+              if (event.key === 'Enter') {
+              this.search()
+              }
+            }}
+          />
+
+            <InputGroup.Addon onClick={() => this.search()}>
+              <Glyphicon glyph="search"/>
+            </InputGroup.Addon>
+          </InputGroup>
+        </FormGroup>
+        <Profile
+          artist={this.state.artist}
+        />
+
+          <div className="Gallery">
+            Gallery
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/profile.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class Profile extends Component {
+  render() {
+    let artist = {name: '', followers: {total: ''}, images: [{url: ''}]}
+    artist = this.props.artist !== null ? this.props.artist : artist;
+
+
+
+    return (
+      <div className="profile">
+        <img
+          alt="Profile"
+          className="profile-img"
+          src={artist.images[0].url}
+        />
+        <div> {artist.name}</div>
+        <div> {artist.followers.total}</div>
+      </div>
+    )
+  }
+}
+
+export default Profile;
